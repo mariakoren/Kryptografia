fe06b0164191f832a8acc5a27c9cfe35a8d6a262
mariakoren <maria.korenn@gmail.com>
2024-04-21 20:22:39 +0200

    small changes

--- laboratorium/lab03/block.py
+++ laboratorium/lab03/block.py
@@ -1,51 +1,38 @@
+# Autor: Maria Koren
+
 from PIL import Image
-import hashlib
-import random
 
-input_image = Image.open("plain.bmp")
-
-
-def save(data, where="images/image.bmp", message="Done"):
+def save(data, where, message="Done"):
     new_data = bytes(data)
-
-    output_image = Image.new("L", input_image.size)  # Create a new grayscale image
-    output_image.putdata(new_data)  # Put the pixel data into the image
+    output_image = Image.new("L", input_image.size)
+    output_image.putdata(new_data)
     output_image.save(where)
     print(message)
 
+def encrypt_image(input_image, key, mode):
+    image_data = input_image.tobytes()
+    size = input_image.size
+    new_data = []
+    if mode == "ECB":
+        for x in range(size[0]):
+            for y in range(size[1]):
+                pp = y * size[0] + x 
+                op = image_data[pp]
+                pta = op ^ key
+                new_data.append(pta)
+    elif mode == "CBC":
+        new_data = [image_data[0] ^ key]
+        for x in range(size[0]*size[1] - 1):
+            new_data.append(new_data[x] ^ image_data[x + 1] ^ key)
 
-block_size = 8
-image_data = input_image.tobytes()
-size = input_image.size
+    new_data = new_data[:size[0]*size[1]]
+    return new_data
 
-# ECB
-new_data = []
-keys = []
-for x in range(block_size):
-    # key = hashlib.md5(str(x ** 3 + x).encode("UTF-8")).digest()
-    key = hashlib.md5(str(random.random() * x).encode("UTF-8")).digest()
-    keys.append(key)
-
-for x in range(size[0]):
-    for y in range(size[1]):
-        pp = y * size[0] + x  # pixel position
-        op = image_data[pp]  # original pixel
-        pta = op ^ keys[x % block_size][y % block_size]     # pixel to add
-        new_data.append(pta)
-
-new_data = new_data[:size[0]*size[1]]
-
-save(new_data, "ecb_crypto.bmp", "zapisano ecb")
-
-
-# CBC
-new_key = 13
-new_data = [image_data[0] ^ new_key]
-for x in range(size[0]*size[1] - 1):
-    new_data.append(new_data[x] ^ image_data[x + 1] ^ keys[(x + 1) % 64 // 8][(x + 1) % 8])
-
-new_data = new_data[:size[0]*size[1]]
-
-save(new_data, "cbc_crypto.bmp", "zapisano cbc")
-
+input_image = Image.open("plain.bmp")
+key = "klucz"
+key_bytes = key.encode("UTF-8")
+ecb_encrypted_data = encrypt_image(input_image, key_bytes[0], "ECB")
+save(ecb_encrypted_data, "ecb_crypto.bmp", "zapisano ecb")
+cbc_encrypted_data = encrypt_image(input_image, key_bytes[0], "CBC")
+save(cbc_encrypted_data, "cbc_crypto.bmp", "zapisano cbc")
 print("Done")
